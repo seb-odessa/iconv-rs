3b816bb565e653de95e489b0458f43c1d4c76a6c
seb-odessa <seb@ukr.net>
2017-09-13 21:24:49 +0300

    Investigation

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-lib=iconv");
+    println!("cargo:rustc-link-lib=libc6");
     let bindings = bindgen::Builder::default().header("wrapper.h").generate().expect("Unable to generate bindings");
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings.write_to_file(out_path.join("bindings.rs")).expect("Couldn't write bindings!");
